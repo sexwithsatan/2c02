9d72c2f21107778bba24fe3f71e78623bd7bff12
sex with satan <sexwithsatan@users.sf.net>
2018-08-30 15:59:32 -0700

    delete sample code

--- src/main.js
+++ src/main.js
@@ -1,7 +1,3 @@
-import ms from 'ms';
-import lunchtime from './lunchtime.js';
-import millisecondsUntil from './millisecondsUntil.js';
-
 export default function howLongUntilLunch(hours, minutes) {
 	// lunch is at 12.30
 	if (hours === undefined) hours = 12;
